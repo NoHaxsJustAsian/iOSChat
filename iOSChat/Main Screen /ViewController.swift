98dc5088ed937b150f9e8bb550f44c418d3290df
Win Tongtawee <WinTongtawee@gmail.com>
2023-06-12 00:36:15 -0400

    Update ViewController.swift

--- iOSChat/Main Screen /ViewController.swift
+++ iOSChat/Main Screen /ViewController.swift
@@ -12,55 +12,71 @@ import FirebaseFirestore
 class ViewController: UIViewController {
 
     let mainScreen = MainScreenView()
-    
+
     var usersList = [User]()
-    
+
     var handleAuth: AuthStateDidChangeListenerHandle?
-    
-    var currentUser:FirebaseAuth.User?
-    
+
+    var currentUser: User?
+
     let database = Firestore.firestore()
-    
+
     let childProgressView = ProgressSpinnerViewController()
-    
+
     override func loadView() {
         view = mainScreen
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         //MARK: handling if the Authentication state is changed (sign in, sign out, register)...
         handleAuth = Auth.auth().addStateDidChangeListener{ auth, user in
             if user == nil{
                 //MARK: not signed in...
                 self.currentUser = nil
                 self.mainScreen.labelText.text = "Please sign in to see the chats!"
-                
+
                 //MARK: Reset tableView...
                 self.usersList.removeAll()
                 self.mainScreen.tableViewContacts.reloadData()
-                
+
                 //MARK: Sign in bar button...
                 self.setupRightBarButton(isLoggedin: false)
-                
+
             }else{
                 //MARK: the user is signed in...
-                self.currentUser = user
-                self.mainScreen.labelText.text = "Welcome \(user?.displayName ?? "Anonymous")!"
-                
+                let userData = self.database.collection("users").document(user!.uid)
+                userData.getDocument { (document, error) in
+                    if let document = document, document.exists {
+                        do {
+                            let user = try document.data(as: User.self)
+                            self.currentUser = user
+                            self.mainScreen.labelText.text = "Welcome (user.name)!"
+                        } catch {
+                            print("Error decoding user data: (error)")
+                        }
+                    } else {
+                        print("User document does not exist")
+                    }
+                }
+
                 //MARK: Logout bar button...
                 self.setupRightBarButton(isLoggedin: true)
-                
+
                 //MARK: Observe Firestore database to display the contacts list...
                 self.database.collection("users")
+                    .document((self.currentUser?.name)!)
+                    .collection("contacts")
                     .addSnapshotListener(includeMetadataChanges: false, listener: {querySnapshot, error in
                         if let documents = querySnapshot?.documents{
                             self.usersList.removeAll()
                             for document in documents{
                                 do{
-                                    let users  = try document.data(as: User.self)
-                                    self.usersList.append(users)
+                                    if (document.documentID != self.currentUser?.id) {
+                                        let users  = try document.data(as: User.self)
+                                        self.usersList.append(users)
+                                    }
                                 }catch{
                                     print(error)
                                 }
@@ -69,36 +85,35 @@ class ViewController: UIViewController {
                             self.mainScreen.tableViewContacts.reloadData()
                         }
                     })
-                
             }
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         title = "My Contacts"
-        
+
         //MARK: patching table view delegate and data source...
         mainScreen.tableViewContacts.delegate = self
         mainScreen.tableViewContacts.dataSource = self
-        
+
         //MARK: removing the separator line...
         mainScreen.tableViewContacts.separatorStyle = .none
-        
+
         //MARK: Make the titles look large...
-        navigationController?.navigationBar.prefersLargeTitles = true
+navigationController?.navigationBar.prefersLargeTitles = true
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         Auth.auth().removeStateDidChangeListener(handleAuth!)
     }
-    
+
     func signIn(email: String, password: String){
         Auth.auth().signIn(withEmail: email, password: password)
     }
-    
+
     func openChat(user: User){
         let viewChatViewController = ViewChatViewController()
         viewChatViewController.userSelf = self.currentUser
@@ -106,4 +121,3 @@ class ViewController: UIViewController {
         navigationController?.pushViewController(viewChatViewController, animated: true)
     }
 }
-
